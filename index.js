b10cc25093700db710b84da23f88c14609fa1c5b
Julian Gruber <julian@juliangruber.com>
2013-07-30 16:47:37 -0400

    use obj and once

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+var once = require('once');
+var o = require('obj');
+
 module.exports = getPosition;
 
 function getPosition (opts, fn) {
@@ -6,45 +9,27 @@ function getPosition (opts, fn) {
     opts = {};
   }
 
+  fn = once(fn);
+
   unlessDefined(opts, {
     timeout: 5000,
     maximumAge: 60000,
     enableHighAccuracy: true
   });
 
-  var once = Once();
-  var onposition = once(function (position) {
-    var ret = clone(position.coords);
-    ret.timestamp = position.timestamp;
-    fn(null, ret);
-  });
-  var onerror = once(fn);
+  navigator.geolocation.getCurrentPosition(onposition, fn, opts);
 
-  navigator.geolocation.getCurrentPosition(onposition, onerror, opts);
+  function onposition (position) {
+    fn(null, o(position.coords)
+              .clone()
+              .set('timestamp', position.timestamp)
+              .get());
+  };
 }
 
 function unlessDefined (base, xtend) {
-  Object.keys(xtend).forEach(function (key) {
-    if (typeof base[key] == 'undefined') base[key] = xtend[key];
+  o(xtend).each(function (key, value) {
+    if (typeof base[key] == 'undefined') base[key] = value;
   });
-}
-
-function Once () {
-  var called = false;
-  return function (fn) {
-    return function () {
-      if (called) return;
-      called = true;
-      fn.apply(this, arguments);
-    }
-  }
-}
-
-function clone (obj) {
-  var ret = {};
-  Object.keys(obj).forEach(function (k) {
-    ret[k] = obj[k];
-  });
-  return ret;
 }
 
